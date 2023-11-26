08e96b41c0ba65c862fd8b67ccf12d8ae37faae1
linu <jahn.linus22@gmail.com>
2023-11-26 11:10:04 +0100

    made macro in Grace.c somewhat smarter and got rid of redundancy in variadic arguments

--- ex01/Grace_kid.c
+++ ex01/Grace_kid.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #define FILE "Grace_kid.c"
-#define SELF "#include <stdio.h>%c#define FILE %cGrace_kid.c%c%c#define SELF %c%s%c%c#define ACTION(x) int	main(void) {fprintf(fopen(FILE, %cw%c), SELF, 10, 34, 34, 10, 34, SELF, 34, 10, 34, 34, 10); return (0);}%cACTION(x)"
-#define ACTION(x) int	main(void) {fprintf(fopen(FILE, "w"), SELF, 10, 34, 34, 10, 34, SELF, 34, 10, 34, 34, 10); return (0);}
-ACTION(x)
+#define SELF "#include <stdio.h>%1$c#define FILE %2$cGrace_kid.c%2$c%1$c#define SELF %2$c%3$s%2$c%1$c#define ACTION(mode) int	main(void) {fprintf(fopen(FILE, mode), SELF, 10, 34, SELF); return (0);}%1$cACTION(%2$cw%2$c)%1$c/*%1$c	ljahn, 42-Wolfsburg%1$c*/"
+#define ACTION(mode) int	main(void) {fprintf(fopen(FILE, mode), SELF, 10, 34, SELF); return (0);}
+ACTION("w")
+/*
+	ljahn, 42-Wolfsburg
+*/
